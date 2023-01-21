75a985516a49399eda071bcb3f28f5c19c1ffc2d
dhanraj jangid <djjangid376@gmail.com>
2023-01-22 00:27:29 +0530

    database change

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgCNYY29Fa-sAg4I3_qsB3bEaBgjz1Mv4",
-  authDomain: "fir-crud-9d788.firebaseapp.com",
-  projectId: "fir-crud-9d788",
-  storageBucket: "fir-crud-9d788.appspot.com",
-  messagingSenderId: "1094331005508",
-  appId: "1:1094331005508:web:d3696d2f6382b747071a11",
-  measurementId: "G-JLEPJGTW28"
+  apiKey: "AIzaSyCyrHWYx3oFvuvQs6g3SzL3AVoL4KaniSw",
+  authDomain: "blog-website-810ec.firebaseapp.com",
+  projectId: "blog-website-810ec",
+  storageBucket: "blog-website-810ec.appspot.com",
+  messagingSenderId: "624900235835",
+  appId: "1:624900235835:web:4658067dad6ebf49bfb21e",
+  measurementId: "G-LYHG89JM97"
 };
 
 
